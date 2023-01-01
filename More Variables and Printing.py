6029f61977c2bc7f29529aad9279406829a455a4
adityakt13 <adityakt13@gmail.com>
2023-01-01 19:09:48 +0530

    practice

---
+++ More Variables and Printing.py
@@ -0,0 +1,16 @@
+my_name = 'Aditya kumar Tripathi'
+my_age = 20
+my_height = 178
+my_weight = 70
+my_eyes = 'black'
+my_teeth = 32
+my_hair_color = 'black'
+
+print ("Let's talk about %s." % my_name)
+print ("He is %d." % my_age)
+print ("He is %d cm. tall ." % my_height)
+print ("He is %d kg. heavy." % my_weight)
+print ("His eyes are %s in color." % my_eyes)
+print ("He has a total of %d teeth." % my_teeth)
+print ("He has %s hairs." % my_hair_color)
+print ("If I add %d , %d, %d, %d we will get %d." %( my_age ,my_height,my_weight,my_teeth,my_age + my_height + my_weight + my_teeth))
