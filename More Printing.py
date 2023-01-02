21bd2bf7544116d84c586a0a1a235d4e3afd59bb
adityakt13 <adityakt13@gmail.com>
2023-01-02 12:36:10 +0530

    Create More Printing.py

---
+++ More Printing.py
@@ -0,0 +1,18 @@
+print ("Mary had a little lamp") # prints string
+print ("Its fleece was white as %s" %'snow') # prints string
+print("And everywhere that mary went.") #prints string
+print ("."*15) # prints string "." multiplied 15
+
+end1 = "C" 
+end2 = "H"
+end3 = "E"
+end4 = "E"
+end5 = "S"
+end6 = "E"
+end7 = "B"
+end8 = "U"
+end9 = "R"
+end10 = "G"
+end11= "E"
+end12 = "R"
+print (end1+end2+end3+end4+end5+end6 ,end7+end8+end9+end10+end11+end12) # prints the values of those variables which are part of the print statement
